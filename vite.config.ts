7d2eeddbf5ddd48b31d03c03512d68389efec425
Nico Dinata <nicodnt@icloud.com>
2024-08-03 23:37:48 +1000

    add deployment gh action

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/plcalc/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
